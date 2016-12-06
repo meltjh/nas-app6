0660b72107c205a05d6e21519efa5829e059d915
Melissa Tjhia <melissatjhia@wcw-stud-145-109-31-196.wireless.uva.nl>
2016-12-06 11:20:08 +0100

    Initial Commit

---
+++ melissatjhia-pset6/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  melissatjhia-pset6
+//
+//  Created by Melissa Tjhia on 06-12-16.
+//  Copyright © 2016 Melissa Tjhia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
